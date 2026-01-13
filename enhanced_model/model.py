ff34849d47f60b473f81675446e627c8debbd193
samreedh bhuyan <samreedhbhuyan@samreedhs-macbook.local>
2026-01-13 23:41:37 +0530

    minor changes

--- enhanced_model/model.py
+++ enhanced_model/model.py
@@ -413,7 +413,7 @@ class Dynamic_attention_model(nn.Module):
         
         self.reconstructed_image = ReconstructionUnit(layer1_channels * 6, layer2_channels * 6, layer3_channels * 6, layer4_channels * 6)
     
-    def forward(self, gamma, underexposed, overexposed, original, h_2, histoEQ):
+    def forward(self, gamma, underexposed, overexposed, original, h2, histoEQ):
 
         gamma_l1, gamma_l2, gamma_l3, gamma_l4 = checkpoint(self.gamma_encoder, gamma, use_reentrant=False)
         underexposed_l1, underexposed_l2, underexposed_l3, underexposed_l4 = checkpoint(self.underexposed_encoder, underexposed, use_reentrant=False)
