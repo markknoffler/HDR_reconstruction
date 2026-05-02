22068f67c9ff0ce6201c82dc2dcf4097b1ab0195
Sam Reedh Bhuyan <samreedhbhuyan@example.com>
2026-05-02 12:55:22 +0530

    some bug fixes

--- TriGate-HDR/model/seaming_model/discriminator.py
+++ TriGate-HDR/model/seaming_model/discriminator.py
@@ -3,16 +3,23 @@ import torch.nn as nn
 
 
 def _disc_head(in_ch):
+
     return nn.Sequential(
-        nn.Conv2d(in_ch, 64, 4, 2, 1),
+        nn.Conv2d(in_ch, 64, kernel_size=4, stride=2, padding=1),
         nn.LeakyReLU(0.2, inplace=True),
-        nn.Conv2d(64, 128, 4, 2, 1),
+        nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
         nn.InstanceNorm2d(128),
         nn.LeakyReLU(0.2, inplace=True),
-        nn.Conv2d(128, 256, 4, 2, 1),
+        nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
         nn.InstanceNorm2d(256),
         nn.LeakyReLU(0.2, inplace=True),
-        nn.Conv2d(256, 1, 3, 1, 1),
+        nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1),
+        nn.InstanceNorm2d(128),
+        nn.LeakyReLU(0.2, inplace=True),
+        nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1),
+        nn.InstanceNorm2d(64),
+        nn.LeakyReLU(0.2, inplace=True),
+        nn.Conv2d(64, 1, kernel_size=3, stride=1, padding=1),
     )
 
 
@@ -26,4 +33,3 @@ class SeamingDiscriminator(nn.Module):
         global_score = self.global_head(image)
         seam_score = self.seam_head(torch.cat([image, seam_mask], dim=1))
         return global_score, seam_score
-
